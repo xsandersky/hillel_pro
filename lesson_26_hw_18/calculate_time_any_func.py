42511aa3e8fa6e8d549042beb59ed21d08d713cd
xsandersky <xsandersky@gmail.com>
2023-08-29 19:37:01 +0300

    hw18

---
+++ lesson_26_hw_18/calculate_time_any_func.py
@@ -0,0 +1,39 @@
+import asyncio
+from functools import wraps
+from time import perf_counter
+
+
+def measure_time(func):
+    if type(func) is type(lambda: None):
+        @wraps(func)
+        def main_func(*args, **kwargs):
+            start_time = perf_counter()
+            result = func(*args, **kwargs)
+            end_time = perf_counter()
+            print(f'Длительность функции {func.__name__} = {end_time - start_time:0.9f}')
+            return result
+        return main_func
+    elif type(func) is type(my_coroutine()):
+        @wraps(func)
+        async def main_coroutine(*args, **kwargs):
+            start_time = perf_counter()
+            result = await func(*args, **kwargs)
+            end_time = perf_counter()
+            print(f'Длительность функции {func.__name__} = {end_time - start_time:0.9f}')
+            return result
+        return main_coroutine
+
+
+@measure_time
+def sync():
+    pass
+
+
+@measure_time
+async def my_coroutine():
+    await asyncio.sleep(2)
+
+
+if __name__=='__main__':
+    sync()
+    asyncio.run(my_coroutine())
